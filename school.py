86c0cc4c4b6f01149c8db697b6430923b481a32c
Wemis <88230804+Wemis@users.noreply.github.com>
2021-09-08 07:24:22 +0300

    Create school.py

---
+++ school.py
@@ -0,0 +1,276 @@
+print("Created by WEMIS") 
+i=int(input("Количество уроков ")) 
+q=385
+u=330
+k="Осталось"
+v="Урок закончился"
+r="До конца урока 5 мин"
+z="мин"
+if i==6:
+  print(k, u, z)
+  import time
+  time .sleep(600)
+  print(k, u, z)
+  time .sleep(600)
+  u-=10
+  print(k, u, z)
+  time .sleep(600)
+  u-=10
+  print(k, u, z)
+  time .sleep(600)
+  u-=10
+  print(k, u, z)
+  time .sleep(600)
+  u-=10
+  print(k, u, z)
+  print(r)
+  time .sleep(300)
+  u-=5
+  print(v)
+  time .sleep(600)
+  u-=10
+  print(k, u, z)
+  time .sleep(600)
+  u-=10
+  print(k, u, z)
+  time .sleep(600)
+  u-=10
+  print(k, u, z)
+  time .sleep(600)
+  u-=10
+  print(k, u, z)
+  time .sleep(600)
+  u-=10
+  print(k, u, z)
+  print(r)
+  time .sleep(300)
+  u-=5
+  print(v)
+  time .sleep(600)
+  u-=10
+  print(k, u, z)
+  time .sleep(600)
+  u-=10
+  print(k, u, z)
+  time .sleep(600)
+  u-=10
+  print(k, u, z)
+  time .sleep(600)
+  u-=10
+  print(k, u, z)
+  time .sleep(600)
+  u-=10
+  print(k, u, z)
+  time .sleep(300)
+  u-=5
+  print(r)
+  time .sleep(300)
+  u-=5
+  print(v)
+  time .sleep(600)
+  u-=10
+  print(k, u, z)
+  time .sleep(600)
+  u-=10
+  print(k, u, z)
+  time .sleep(600)
+  u-=10
+  print(k, u, z)
+  time .sleep(600)
+  u-=10
+  print(k, u, z)
+  time .sleep(600)
+  u-=10
+  print(k, u, z)
+  time .sleep(300)
+  u-=5
+  print(r)
+  time .sleep(300)
+  u-=5
+  print(v)
+  u-=10
+  print(k, u, z)
+  time .sleep(600)
+  u-=10
+  print(k, u, z)
+  time .sleep(600)
+  u-=10
+  print(k, u, z)
+  time .sleep(600)
+  u-=10
+  print(k, u, z)
+  time .sleep(600)
+  u-=10
+  print(k, u, z)
+  time .sleep(300)
+  u-=5
+  print(r)
+  time .sleep(300)
+  u-=5
+  print(v)
+  time .sleep(600)
+  u-=10
+  print(k, u, z)
+  time .sleep(600)
+  u-=10
+  print(k, u, z)
+  time .sleep(600)
+  u-=10
+  print(k, u, z)
+  time .sleep(600)
+  u-=10
+  print(k, u, z)
+  time .sleep(600)
+  u-=10
+  print(k, u, z)
+  time .sleep(300)
+  u-=5
+  print(r)
+  time .sleep(300)
+  u-=5
+  print(v)
+  time .sleep(300)
+  print("Уроки закончились, можно идти домой😉")
+if i==7:
+  print(k, q, z)
+  import time
+  time .sleep(600)
+  q-=10
+  print(k, q, z)
+  time .sleep(600)
+  q-=10
+  print(k, q, z)
+  time .sleep(600)
+  q-=10
+  print(k, q, z)
+  time .sleep(600)
+  q-=10
+  print(k, q, z)
+  time .sleep(600)
+  q-=10
+  print(k, q, z)
+  print(r)
+  time .sleep(300)
+  q-=5
+  print(v)
+  time .sleep(600)
+  q-=10
+  print(k, q, z)
+  time .sleep(600)
+  q-=10
+  print(k, q, z)
+  time .sleep(600)
+  q-=10
+  print(k, q, z)
+  time .sleep(600)
+  q-=10
+  print(k, q, z)
+  time .sleep(600)
+  q-=10
+  print(k, q, z)
+  print(r)
+  time .sleep(300)
+  q-=5
+  print(v)
+  time .sleep(600)
+  q-=10
+  print(k, q, z)
+  time .sleep(600)
+  q-=10
+  print(k, q, z)
+  time .sleep(600)
+  q-=10
+  print(k, q, z)
+  time .sleep(600)
+  q-=10
+  print(k, q, z)
+  time .sleep(600)
+  q-=10
+  print(k, q, z)
+  time .sleep(300)
+  q-=5
+  print(r)
+  time .sleep(300)
+  q-=5
+  print(v)
+  time .sleep(600)
+  q-=10
+  print(k, q, z)
+  time .sleep(600)
+  q-=10
+  print(k, q, z)
+  time .sleep(600)
+  q-=10
+  print(k, q, z)
+  time .sleep(600)
+  q-=10
+  print(k, q, z)
+  time .sleep(600)
+  q-=10
+  print(k, q, z)
+  time .sleep(300)
+  q-=5
+  print(r)
+  time .sleep(300)
+  q-=5
+  print(v)
+  q-=10
+  print(k, q, z)
+  time .sleep(600)
+  u-=10
+  print(k, q, z)
+  time .sleep(600)
+  q-=10
+  print(k, q, z)
+  time .sleep(600)
+  q-=10
+  print(k, q, z)
+  time .sleep(600)
+  q-=10
+  print(k, q, z)
+  time .sleep(300)
+  q-=5
+  print(r)
+  time .sleep(300)
+  q-=5
+  print(v)
+  time .sleep(600)
+  q-=10
+  print(k, q, z)
+  time .sleep(600)
+  q-=10
+  print(k, q, z)
+  time .sleep(600)
+  q-=10
+  print(k, q, z)
+  time .sleep(600)
+  q-=10
+  print(k, q, z)
+  time .sleep(600)
+  q-=10
+  print(k, q, z)
+  time .sleep(300)
+  print(r)
+  time .sleep(300)
+  q-=5
+  print(v)
+  time .sleep(600)
+  q-=10
+  print(k, q, z)
+  time .sleep(600)
+  q-=10
+  print(k, q, z)
+  time .sleep(600)
+  q-=10
+  print(k, q, z)
+  time .sleep(600)
+  q-=10
+  print(k, q, z)
+  time .sleep(600)
+  q-=10
+  print(k, q, z)
+  print(r)
+  time .sleep(300)
+  print(v)
+  time .sleep(300)
+  print("Можно идти домой😉")
